935095843a93c93270f3b5825829b1710bcaef34
POLLY1515 <amarantepolly2030@gmail.com>
2025-06-28 11:00:30 -0300

    Project created

---
+++ backend/src/main/java/com/poliana/dasbordVendas/DasbordVendasApplication.java
@@ -0,0 +1,13 @@
+package com.poliana.dasbordVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasbordVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasbordVendasApplication.class, args);
+	}
+
+}
