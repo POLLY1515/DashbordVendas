f21d3a8c8b71d5a1b5f7d677cf13a03d780bb454
POLLY1515 <amarantepolly2030@gmail.com>
2025-06-29 06:28:07 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import DataTable from "./components/DataTable"
+import Footer from "./components/Footer"
+import NavBar from "./components/NaveBar"
 
 
 function App() {
 
   return (
     <>
-    <h1 className="text-primary">ola mundo!</h1>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">ola mundo!</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
     </>
   )
 }
