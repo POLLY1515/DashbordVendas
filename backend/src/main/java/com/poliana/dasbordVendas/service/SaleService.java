7c26de2b0302bcff1570b6155b0e07662ae497f8
POLLY1515 <amarantepolly2030@gmail.com>
2025-07-03 09:07:17 -0300

    Group by search

--- backend/src/main/java/com/poliana/dasbordVendas/service/SaleService.java
+++ backend/src/main/java/com/poliana/dasbordVendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.poliana.dasbordVendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.poliana.dasbordVendas.dto.SaleDTO;
+import com.poliana.dasbordVendas.dto.SaleSuccessDTO;
+import com.poliana.dasbordVendas.dto.SaleSumDTO;
 import com.poliana.dasbordVendas.entities.Sale;
 import com.poliana.dasbordVendas.repositories.SaleRepository;
 import com.poliana.dasbordVendas.repositories.SellerRepository;
@@ -27,6 +31,14 @@ public class SaleService {
 		return result.map(x -> new SaleDTO(x));
 	}
 	
-	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO>successGroupedBySeller(){
+		return repository.sucessGroupedBySeller();
+	}
 
 }
